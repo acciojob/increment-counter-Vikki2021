082d925c1f8ae8a6fbf7f5d41b9577723d9b08e5
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-03-09 13:17:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let count=0
+function SubmitBtn() {
+	let counter = document.getElementById("counter");
+	alert(count);
+	count++;
+	counter.textContent = `${count+1}`
+}
